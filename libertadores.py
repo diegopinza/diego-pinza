2f2885d14c54792d38aad31de205b6c430ebc83d
diegopinza <133123758+diegopinza@users.noreply.github.com>
2023-05-24 08:25:07 -0500

    Update libertadores.py

--- libertadores.py
+++ libertadores.py
@@ -1,24 +1,49 @@
-import pandas as pd
 
-# Cargar datos de los deportistas colombianos en los Juegos Olímpicos
-data = pd.read_csv('deportistas_colombianos.csv')
 
-# Filtrar los deportistas por medallas ganadas
-medallistas = data[data['Medallas'] > 0]
+# Creación de las variables para los puntajes
+river_puntaje = 0
+boca_puntaje = 0
 
-# Ordenar los deportistas por año de participación
-medallistas = medallistas.sort_values('Año')
+# Historial de enfrentamientos
+enfrentamientos = [
+    {"rival": "Boca Juniors", "local": "River Plate", "puntos_local": 0, "puntos_rival": 0},
+    {"rival": "River Plate", "local": "Boca Juniors", "puntos_local": 0, "puntos_rival": 0},
+    # Agrega más enfrentamientos aquí según la historia de la Copa Libertadores
+]
 
-# Imprimir la información de los deportistas medallistas
-for index, deportista in medallistas.iterrows():
-    print(f"Deportista: {deportista['Nombre']}")
-    print(f"Año: {deportista['Año']}")
-    print(f"Medalla: {deportista['Medallas']}")
-    print("")
+# Simulación de los partidos
+for enfrentamiento in enfrentamientos:
+    local = enfrentamiento["local"]
+    rival = enfrentamiento["rival"]
+   
+    # Simulación de los puntajes
+    puntos_local = random.randint(0, 5)
+    puntos_rival = random.randint(0, 5)
+   
+    enfrentamiento["puntos_local"] = puntos_local
+    enfrentamiento["puntos_rival"] = puntos_rival
+   
+    # Actualización de los puntajes totales
+    if local == "River Plate":
+        river_puntaje += puntos_local
+        boca_puntaje += puntos_rival
+    else:
+        river_puntaje += puntos_rival
+        boca_puntaje += puntos_local
 
-# Calcular el total de medallas ganadas por Colombia
-total_medallas = medallistas['Medallas'].sum()
-print(f"Total de medallas ganadas por Colombia: {total_medallas}"
+# Imprimir resultados
+print("Historia de enfrentamientos en la Copa Libertadores entre River Plate y Boca Juniors:")
+for enfrentamiento in enfrentamientos:
+    local = enfrentamiento["local"]
+    rival = enfrentamiento["rival"]
+    puntos_local = enfrentamiento["puntos_local"]
+    puntos_rival = enfrentamiento["puntos_rival"]
+   
+    print(f"{local} {puntos_local} - {puntos_rival} {rival}")
+
+print("\nPuntaje total:")
+print(f"River Plate: {river_puntaje} puntos")
+print(f"Boca Juniors: {boca_puntaje} puntos")
 #DANNY_URBINA
 #DEPORTES
 #IEM_ESCUELA_NORMAL_PASTO
